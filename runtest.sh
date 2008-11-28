8bc1f8c3a57b02f2e89128506c94fc124393110b
Ryoichi KATO <ryo1kato@gmail.com>
2008-11-28 10:31:34 +0900

    --ignore-undef support

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
 
-./ets test/test.config test/test.template
+TEST_CASES="test undef"
+
+
+runtest () {
+    local testname=$1
+    local option
+
+    case $testname in
+        undef) option="--ignore-undef" ;;
+        *)     option=""
+    esac
+
+    ./ets $option test/$testname.config test/$testname.template |
+        diff -u test/$testname.expected -
+}
+
+
+echo "Running tests ..."
+for test in $TEST_CASES
+do
+    echo -n "* $test ... "
+    if runtest $test
+    then
+        echo OK
+    else
+        echo FAIL
+    fi
+done

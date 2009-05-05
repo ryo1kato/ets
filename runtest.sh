6e479a0a31124baf5b5655545b1044703f09d707
Ryoichi KATO <ryo1kato@gmail.com>
2009-05-06 00:32:22 +0900

    __TEMPLATE_FILE__ support

--- runtest.sh
+++ runtest.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
-TEST_CASES="test undef"
+TEST_CASES="test undef embed"
+FAIL_TEST_CASES=""
+PYTHONS="python2.4 python2.5"
 
 
 runtest () {
     local testname=$1
-    local option
+    local option=""
+    local config="test/$testname.config"
+    local template="test/$testname.template"
+
 
     case $testname in
         undef) option="--ignore-undef" ;;
-        *)     option=""
+        embed) template="" ;;
+        *) : ;;
     esac
 
-    ./ets $option test/$testname.config test/$testname.template |
-        diff -u test/$testname.expected -
+    for python in $PYTHONS
+    do
+        echo ---------- $python -----------
+        $python ./ets $option $config $template |
+            diff -u test/$testname.expected -
+    done
 }
 
 
@@ -21,9 +31,23 @@ echo "Running tests ..."
 for test in $TEST_CASES
 do
     echo -n "* $test ... "
-    if runtest $test
+    if runtest $test > test-$test.log 2>&1
     then
         echo OK
+        rm -f test-$test.log
+    else
+        echo FAIL
+    fi
+done
+
+
+for ftest in $FAIL_TEST_CASES
+do
+    echo -n "* $ftest ... "
+    if ! runtest $ftest > ftest-$ftest.log 2>&1
+    then
+        echo OK
+        rm -f ftest-$ftest.log
     else
         echo FAIL
     fi
